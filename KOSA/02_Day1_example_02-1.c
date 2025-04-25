a34797108151f4d13c3acb454ed60b4d986b5bcc
hong000321 <hong000321@gmail.com>
2025-04-25 14:01:00 +0900

    [KOSA|etc] 파일명 수정

---
+++ KOSA/02_Day1_example_02-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "Test_header.h"
+
+
+#if FILE_NUM == 2
+int my_main(void) {
+	printf("È«´ë¿À\nÈ« ´ë ¿À\nÈ«  ´ë  ¿À\n");
+	return 0;
+}
+#endif
---
+++ KOSA/09_Day2_CommaOp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "Test_header.h"
+
+
+#if FILE_NUM == 9
+int my_main(void) {
+	int num1 = 1;
+	int num2 = 2;
+	printf("Hello "), printf("world!  \n");
+	num1++, num2++;
+	printf("%d ", num1), printf("%d ", num2), printf("\n");
+
+
+	return 0;
+}
+#endif
