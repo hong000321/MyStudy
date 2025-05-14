5cef539a9ecb454d34eecd7d4e040597cd3c4a2d
DaeO Hong <hong000321@gmail.com>
2025-05-14 13:31:45 +0900

    [algorithm|etc] 중복된 파일 삭제

--- KOSA/C++/Chapter2/26_CH2_reinterpret_cast.cpp
+++ KOSA/C++/Chapter2/26_CH2_reinterpret_cast.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 int main(void){
-    int num = 0x040204;
+    int num = 0x040704;
     char *ptr = reinterpret_cast<char *>(&num);
     cout << static_cast<int>(*(ptr+1)) << endl;
 
